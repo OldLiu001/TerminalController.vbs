56e1116df0415bf3c5e1ae85527a262b082bbbfa
老刘 <632171029@qq.com>
2024-06-17 11:04:05 +0800

    add hanoi draw logic

--- Examples/Hanoi.js
+++ Examples/Hanoi.js
@@ -9,8 +9,39 @@ var BufW = Terminal.ColumnLength;
 var BufH = Terminal.RowLength;
 
 // Draw the Hanoi Tower
+//test the DrawTower function
+WScript.Echo(DrawTower([0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]).join('\n'));
+WScript.Echo(DrawTower([1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]).join('\n'));
+WScript.Echo(DrawTower([0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]).join('\n'));
 
 
+
+
+
+function DrawTower(a, b, c) {
+	var TowerTemplate = [
+		['     ©à     ', '     ©¦     '],
+		['    ©¤©à©¤    ', '     ©¦     '],
+		['   ©¤©¤©à©¤©¤   ', '     ©¦     '],
+		['  ©¤©¤©¤©à©¤©¤©¤  ', '     ©¦     '],
+		['¨\¨T¨T¨T¨T¨k¨T¨T¨T¨T¨_', '¨\¨T¨T¨T¨T¨k¨T¨T¨T¨T¨_']
+	];
+
+	var Tower = [];
+	var Spaces = '  ';
+	for (var i = 1; i <= 4; i++) {
+		Tower.push(
+			TowerTemplate[i - 1][a[i - 1]] + Spaces +
+			TowerTemplate[i - 1][b[i - 1]] + Spaces +
+			TowerTemplate[i - 1][c[i - 1]]);
+	}
+	// Append the base
+	Tower.push(
+		TowerTemplate[4][0] + Spaces +
+		TowerTemplate[4][0] + Spaces +
+		TowerTemplate[4][0]);
+	return Tower;
+}
 
 
 function PlayHanoi(n, a, b, c) {
